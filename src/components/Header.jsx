4352b2f870aebad5e4d550fcb15a95853372c331
Faisal A F Rahman <afaisalalmunawar0315@gmail.com>
2023-10-08 18:50:51 +0700

    fix on className

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import SearchBar from "./HeaderSearch";
 
 const Header = ({ onSearch }) => {
     return (
-        <div className="notes-app__header">
+        <div className="note-app__header">
             <h1>Dicatatin</h1>
             <SearchBar onSearch={onSearch} />
         </div>
