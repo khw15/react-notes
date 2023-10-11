31dab3a0e99acac4c1ea18e868d2dbc9830fecbe
Faisal A F Rahman <afaisalalmunawar0315@gmail.com>
2023-10-11 19:41:01 +0700

    add layout components

---
+++ src/components/layout/NotFoundMessage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NotFoundMessage() {
+    return (
+        <>
+            <h1>404</h1>
+            <p>Page not found</p>
+        </>
+    )
+};
