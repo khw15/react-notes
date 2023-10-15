d5507b3890747af58eb2d3da050c44bed216115e
Faisal A F Rahman <afaisalalmunawar0315@gmail.com>
2023-10-15 19:13:38 +0700

    beautifying no note pages

--- src/components/notes/NoteListEmpty.jsx
+++ src/components/notes/NoteListEmpty.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 
 export default function NoteListEmpty() {
   return (
-    <section className="notes-list-empty">
-      <p className="notes-list__empty">No notes here.</p>
+    <section className="notes-list-empty" style={styles.container}>
+      <p className="notes-list__empty" style={styles.text}>
+        No notes here.
+      </p>
     </section>
   )
+}
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    marginTop: '20vh',
+    fontFamily: 'Quicksand, Arial, sans-serif',
+    color: '#555'
+  },
+  text: {
+    fontSize: '1.5rem',
+    fontWeight: 'bold',
+    color: '#FF5733'
+  }
 }
