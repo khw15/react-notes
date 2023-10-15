42e42a71a80857bf594d649c61627171f7065cd6
Faisal A F Rahman <afaisalalmunawar0315@gmail.com>
2023-10-15 19:08:18 +0700

    beautifying 404 message

--- src/components/layout/NotFoundMessage.jsx
+++ src/components/layout/NotFoundMessage.jsx
@@ -2,9 +2,32 @@ import React from 'react'
 
 export default function NotFoundMessage() {
   return (
-    <>
-      <h1>404</h1>
-      <p>Page not found</p>
-    </>
+    <div style={styles.container}>
+      <span role="img" aria-label="Sad Emoji" style={styles.emoji}>
+        😢😢😢
+      </span>
+      <h1 style={styles.header}>404</h1>
+      <p style={styles.paragraph}>Oops! Page not found</p>
+    </div>
   )
+}
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    marginTop: '20vh',
+    fontFamily: 'Quicksand, sans-serif',
+    color: 'white'
+  },
+  emoji: {
+    fontSize: '3rem'
+  },
+  header: {
+    fontSize: '4rem',
+    color: '#FF5733'
+  },
+  paragraph: {
+    fontSize: '1.5rem',
+    color: '#FF5733'
+  }
 }
