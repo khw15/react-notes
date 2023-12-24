6fc844c0a946109707aac979f4789e9db645426c
Faisal A F Rahman <afaisalalmunawar0315@gmail.com>
2023-12-25 02:01:57 +0700

    Refactor NotFoundMessage component and update font styles

--- src/components/layout/NotFoundMessage.jsx
+++ src/components/layout/NotFoundMessage.jsx
@@ -5,9 +5,32 @@ export default function NotFoundMessage() {
   const text = useLanguage('app')
 
   return (
-    <>
-      <h2>404</h2>
-      <p>{ text.pageNotFound }</p>
-    </>
+    <div style={styles.container}>
+      <span role="img" aria-label="Sad Emoji" style={styles.emoji}>
+        😢😢😢
+      </span>
+      <h1 style={styles.header}>404</h1>
+      <p style={styles.paragraph}>{ text.pageNotFound }</p>
+    </div>
   )
+}
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    marginTop: '20vh',
+    fontFamily: 'Quicksand, sans-serif',
+    color: 'white'
+  },
+  emoji: {
+    fontSize: '3rem'
+  },
+  header: {
+    fontSize: '4rem',
+    color: '#FF5733'
+  },
+  paragraph: {
+    fontSize: '1.5rem',
+    color: '#FF5733'
+  }
 }
