ecde1ccacd4665068511e3f336536e100be536f7
Faisal A F Rahman <afaisalalmunawar0315@gmail.com>
2023-12-25 04:31:30 +0700

    Add support for dynamic locale in showFormattedDate function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,18 +1,26 @@
+import Cookies from 'js-cookie'
+
 const showFormattedDate = (date) => {
+  const locale = Cookies.get('locale') || 'en'
+
   const options = {
     weekday: 'long',
     year: 'numeric',
     month: 'long',
     day: 'numeric'
   }
-  return new Date(date).toLocaleDateString('id-ID', options)
+
+  // Set locale based on the cookie value
+  // eslint-disable-next-line max-len
+  const localeString = locale === 'id' ? 'id-ID' : (locale === 'ko' ? 'ko-KR' : 'en-US')
+
+  return new Date(date).toLocaleDateString(localeString, options)
 }
 
 const capitalizeFirstLetter = (string) =>
   string.charAt(0).toUpperCase() + string.slice(1)
 
-const extractContent = (html) => new DOMParser()
-    .parseFromString(html, 'text/html')
-    .documentElement.textContent
+const extractContent = (html) =>
+  new DOMParser().parseFromString(html, 'text/html').documentElement.textContent
 
 export {showFormattedDate, capitalizeFirstLetter, extractContent}
