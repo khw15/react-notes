8bb8bd166a7491011cb16e42cd5d9e6b3b32b340
Faisal A F Rahman <afaisalalmunawar0315@gmail.com>
2023-12-25 00:48:10 +0700

    theme stored in cookie

--- src/components/layout/ThemeToggler.jsx
+++ src/components/layout/ThemeToggler.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect} from 'react'
 import {GrSun} from 'react-icons/gr'
 import {IoMdMoon} from 'react-icons/io'
 import useTheme from '../../hooks/useTheme'
@@ -6,15 +6,18 @@ import useTheme from '../../hooks/useTheme'
 export default function ThemeToggler() {
   const [theme, changeTheme] = useTheme()
 
+  useEffect(() => {
+    // Apply the theme on mount
+    document.documentElement.setAttribute('data-theme', theme)
+  }, [theme])
+
   return (
     <button
       type="button"
       className="toggle-theme"
       onClick={() => changeTheme(theme === 'dark' ? 'light' : 'dark')}
     >
-      {
-        theme === 'dark' ? <IoMdMoon /> : <GrSun />
-      }
+      {theme === 'dark' ? <IoMdMoon /> : <GrSun />}
     </button>
   )
 }
--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -1,14 +1,28 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+import Cookies from 'js-cookie'
 
 function useTheme() {
   const [theme, setTheme] = useState('dark')
 
+  useEffect(() => {
+    const savedTheme = Cookies.get('theme')
+    if (savedTheme) {
+      setTheme(savedTheme)
+      applyTheme(savedTheme)
+    }
+  }, []) // Run only once on mount
+
   const changeTheme = (val) => {
     setTheme(val)
+    applyTheme(val)
+    Cookies.set('theme', val, {expires: 365})
+  }
+
+  const applyTheme = (val) => {
     const root = window.document.documentElement
     root.setAttribute('data-theme', val)
-    localStorage.setItem('theme', val)
   }
+
   return [theme, changeTheme]
 }
 
